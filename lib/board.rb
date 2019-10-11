bedbd6a4506c3d0fb8a360422f4a02bc1ad5073e
J Resilient <j.resilient.1@gmail.com>
2019-10-11 18:10:23 -0400

    Add #make_move and #next_turn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
@@ -19,10 +20,26 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    start_pos -=1 if start_pos.between?(1,6)
+    @op_store = current_player_name == @name1 ? 13 : 6
+    stones = cups[start_pos]
+    current_idx = start_pos
+
+    until stones.empty?
+      current_idx = (current_idx + 1) <= 13 ? (current_idx + 1) : 0
+      next if current_idx == @op_store
+      cups[current_idx] << stones.pop
+    end
+
+    render
+    return next_turn(current_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    return :prompt if (ending_cup_idx == 13 || ending_cup_idx == 6) && ending_cup_idx != @op_store
+    return :switch if cups[ending_cup_idx].length == 1
+    ending_cup_idx
   end
 
   def render
